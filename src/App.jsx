c45b65cca82433b1aae07b9b5d837aa61601fd6d
Oti David Chibueze <davidchibueze47@gmail.com>
2024-09-02 11:18:48 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  return (
+    <div>
+      <DateCounter />
+    </div>
+  );
+};
+
+const DateCounter = () => {
+  const [inputCount, setInputCount] = useState(0);
+  const [inputRange, setInputRange] = useState(1);
+
+  const date = new Date();
+  date.setDate(date.getDate() + inputCount);
+
+  const handleReset = () => {
+    setInputCount(0);
+    setInputRange(1);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <input
+        type="range"
+        value={inputRange}
+        min={1}
+        max={10}
+        onChange={(e) => setInputRange(e.target.value)}
+      />
+      <span>{inputRange}</span>
 
-export default App
+      <div>
+        <button onClick={() => setInputCount((i) => i - inputRange)}>-</button>
+        <input
+          type="text"
+          value={inputCount}
+          onChange={(e) => setInputCount(Number(e.target.value))}
+        />
+        <button onClick={() => setInputCount((i) => i + inputRange)}>+</button>
+      </div>
+      <div>
+        <p>
+          <span>
+            {inputCount === 0
+              ? `today is ${date.toDateString()}`
+              : inputCount > 0
+              ? `${inputCount} days from today is ${date.toDateString()}`
+              : `${Math.abs(
+                  inputCount
+                )} day from today was ${date.toDateString()}`}
+          </span>
+        </p>
+        {inputCount !== 0 || inputRange !== 1 ? (
+          <button onClick={handleReset}>RESET</button>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default App;
